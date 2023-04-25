d29ff0deea16c24cb4397cd167f58cb39af91d7b
iamhung <43690592+lequochung19971@users.noreply.github.com>
2023-04-25 09:30:10 +0700

    feat: intergrate with Azure Database

--- apps/cms/config/database.ts
+++ apps/cms/config/database.ts
@@ -1,3 +1,5 @@
+// Localhost Database
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ env }) => ({
   connection: {
     client: "postgres",
@@ -11,3 +13,25 @@ export default ({ env }) => ({
     },
   },
 });
+
+// Azure Database
+// eslint-disable-next-line import/no-anonymous-default-export
+// export default ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env(
+//         "DATABASE_HOST",
+//         "ecommerce-postgresql-db-dev.postgres.database.azure.com"
+//       ),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "ecommerce-db"),
+//       user: env(
+//         "DATABASE_USERNAME",
+//         "lequochung19971@ecommerce-postgresql-db-dev"
+//       ),
+//       password: env("DATABASE_PASSWORD", "Lequochung@12"),
+//       ssl: env.bool("DATABASE_SSL", true),
+//     },
+//   },
+// });
