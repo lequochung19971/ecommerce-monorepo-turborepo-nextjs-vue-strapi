8eaf37f27333e9f80d04288d46a7a166282ded3a
iamhung <43690592+lequochung19971@users.noreply.github.com>
2023-09-24 16:03:17 +0700

    chore: remove redundant code and add tailwindcss

---
+++ apps/oms/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
